c91a4f07c32d1dd5739478c4ec1c1242cee0e7ea
DrissDL <115555391+DrissDL@users.noreply.github.com>
2023-11-29 12:39:24 +0100

    Door controller (Unlock lock)

--- Assets/Scripts/ControllerDoor.cs
+++ Assets/Scripts/ControllerDoor.cs
@@ -7,24 +7,40 @@ public class ControllerDoor : MonoBehaviour
     [SerializeField] private Sprite closeDoor;
     [SerializeField] private Sprite openDoor;
     [SerializeField] private SpriteRenderer m_renderer;
-    [SerializeField] private bool doorLock = false;
+    [SerializeField] private bool doorLock;
+    [SerializeField] private Collider2D box;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        m_renderer.sprite = closeDoor;
+        doorLock = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        ControlDoor();
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if ((collision.tag == "Player") && (!doorLock))
         {
+            m_renderer.sprite = openDoor;
+        }
+    }
+
+    public void ControlDoor()
+    {
+        if (doorLock)
+        {
+            box.isTrigger = false;
+            m_renderer.sprite = closeDoor;
+        }
+        else
+        {
+            box.isTrigger = true;
             m_renderer.sprite = openDoor;
         }
     }
