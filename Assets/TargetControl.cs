e7fd86d187ca895ec9dda212ac2b761a506dc6c6
DrissDL <115555391+DrissDL@users.noreply.github.com>
2023-12-03 20:20:13 +0100

    Final Boss debut

---
+++ Assets/TargetControl.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetControl : MonoBehaviour
+{
+    public int overload;
+    public bool isOverload = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Fireball") {
+
+        }
+    }
+}
