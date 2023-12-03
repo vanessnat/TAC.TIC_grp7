4f007f71995aa683fea06aa37e2e437166b2fd3d
chloaye <124672851+chloaye@users.noreply.github.com>
2023-12-03 14:52:29 +0100

    MessagePanel

--- Assets/Scripts/ValidateMessage.cs
+++ Assets/Scripts/ValidateMessage.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ValidateMessage : MonoBehaviour
 {
+    [SerializeField] Text whereText;
+    [SerializeField] Text whatText;
+    [SerializeField] Text howText;
+
    public void Validate()
     {
-        Debug.Log("MESSAGE VALIDE");
+        Debug.Log(whereText.text + " ; " + whatText.text + " ; " + howText.text);
     }
 }
