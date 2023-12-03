cbd6e2ee2ce93010cc8c17a16980e9262d296812
DrissDL <115555391+DrissDL@users.noreply.github.com>
2023-12-03 20:20:30 +0100

    Update TargetControl.cs

--- Assets/TargetControl.cs
+++ Assets/TargetControl.cs
@@ -21,7 +21,7 @@ public class TargetControl : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Fireball") {
+        if (collision.CompareTag("Fireball")) {
 
         }
     }
