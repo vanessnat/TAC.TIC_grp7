a9b485704b6ea8e1c668ac74ac66e928b8604eeb
DrissDL <115555391+DrissDL@users.noreply.github.com>
2023-12-04 15:11:53 +0100

    Delete MenuCredits.cs

--- Assets/MenuCredits.cs
+++
@@ -1,9 +0,0 @@
-
-using UnityEngine;
-using UnityEngine.SceneManagement;
-public class MenuCredits : MonoBehaviour
-{
-    public void LoadScene(string Credits){
-        SceneManager.LoadScene(Credits);
-    }
-}
