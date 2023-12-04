44c96e9596af33a4960d12e43bd1af7fb297c5f1
chloaye <124672851+chloaye@users.noreply.github.com>
2023-12-04 16:57:45 +0100

    Messages V1

--- Assets/Scripts/ValidateMessage.cs
+++ Assets/Scripts/ValidateMessage.cs
@@ -5,12 +5,33 @@ using UnityEngine.UI;
 
 public class ValidateMessage : MonoBehaviour
 {
-    [SerializeField] Text whereText;
-    [SerializeField] Text whatText;
-    [SerializeField] Text howText;
+    [SerializeField] List<Text> messageElements = new List<Text>();
+    [SerializeField] GameObject messagePrefab = null;
+    [SerializeField] GameObject player = null;
+    [SerializeField] GameObject worlds = null;
 
    public void Validate()
     {
-        Debug.Log(whereText.text + " ; " + whatText.text + " ; " + howText.text);
+        string message = "";
+        for (int i = 0; i < messageElements.Count; i ++) 
+        {
+            message += messageElements[i].text + " ";
+        }
+
+        GameObject newMessage = Instantiate(messagePrefab);
+        newMessage.transform.position = player.transform.position;
+
+        newMessage.GetComponent<Dialog>().SetOneLineDialog(message);
+
+        foreach (Transform currentChild in worlds.transform)
+        {
+            if (currentChild.gameObject.activeSelf)
+            {
+                newMessage.transform.parent = currentChild;
+            }
+        }
+        
+        //Debug.Log(message);
+        //Debug.Log(whereText.text + " ; " + whatText.text + " ; " + howText.text);
     }
 }
